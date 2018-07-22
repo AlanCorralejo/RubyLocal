6c24e8fc183cd7f4a997c05abd82e46ac9dbb480
AlanCorralejo <munoz.alan9810@gmail.com>
2018-07-21 18:13:37 -0600

    Add files via upload

---
+++ config/routes.rb
@@ -0,0 +1,27 @@
+Rails.application.routes.draw do
+
+
+	resources :articles
+	#=begin
+	#	get "/articles"
+	#	post "/articles"
+	#	delete "/articles"
+	#	get "/articles/:id"
+	#	get "/articles/new"
+	#	get "/articles/:id/edit"
+	#	patch "/articles/:id"
+	#	put "/articles/:id"
+	
+		
+#	end
+		
+		
+		
+	
+		
+
+
+  get 'bienvenido/index'
+  root 'bienvenido#index'
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
